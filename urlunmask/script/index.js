6bf81556597f61db5361d5b02c3bc35b98be3953
Nameesh <nameesh.kumar@optiv.com>
2022-04-08 11:17:09 +0530

    never done

--- urlunmask/script/index.js
+++ urlunmask/script/index.js
@@ -1,3 +1,4 @@
 document.getElementById('inputstring').addEventListener('input', () => {
     document.getElementById("outputstring").value = document.getElementById("inputstring").value.replace(/\(slash\)/g, "\/").replace(/\(dot\)/g, "\.");
 })
+
